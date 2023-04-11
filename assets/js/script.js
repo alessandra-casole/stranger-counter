2e0431a631e8cf76a0cfe027999c6d98f69c1ad6
alessandra-casole <alessandra.casole92@gmail.com>
2023-04-11 16:01:07 +0200

    last commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
+// It contains all app operations and is invoked by window.onload
 function setUpEvents() {
+   const openApp = document.querySelector(".open-app");
+   const counterApp = document.querySelector(".counter");
+   const appImg = document.querySelector(".app-img img");
 
-const openApp = document.querySelector(".open-app");
-const counterApp = document.querySelector(".counter");
-const appImg = document.querySelector(".app-img img");
-
-const imgsArray = [
+   const imgsArray = [
    "./assets/img/c-letter.jpeg",
    "./assets/img/o-letter.jpeg",
    "./assets/img/u-letter.jpeg",
@@ -13,98 +13,100 @@ const imgsArray = [
    "./assets/img/e-letter.jpeg",
    "./assets/img/r-letter.jpeg",
    "./assets/img/counter.jpeg",
-];
+   ];
 
-const count = document.querySelector(".count");
-let countValue = 0;
-count.textContent = countValue;
-
-const btnsContainer = document.querySelector(".buttons-container");
-
-const decrementBtn = document.createElement("button");
-decrementBtn.setAttribute("class", "minus");
-decrementBtn.textContent = "-";
-btnsContainer.append(decrementBtn);
-
-const incrementBtn = document.createElement("button");
-incrementBtn.setAttribute("class", "plus");
-incrementBtn.textContent = "+";
-btnsContainer.append(incrementBtn);
-
-const resetBtn = document.querySelector(".reset");
-
-// Functions + events
-
-function displayApp() {
-   openApp.classList.add("hidden");
-   counterApp.classList.remove("hidden");
-   document.body.style.background = "linear-gradient(to bottom, hsl(217deg 57% 6%), #350a09, hsl(217deg 57% 6%) )";
-   const themeSound = new Audio("./assets/audio/theme.mp3");
-   themeSound.volume = 0.02;
-   themeSound.play();
-}
-
-openApp.addEventListener("click", () => {
-   displayApp();
-});
- 
-// Increase
-function increase() {
-   countValue++;
+   // Count value initially set to 0
+   const count = document.querySelector(".count");
+   let countValue = 0;
    count.textContent = countValue;
-   
-   const incrementSound = new Audio("./assets/audio/flickering.mp3");
-   incrementSound.play();
 
-   for (let i = 0; i < imgsArray.length; i++) {
-      if ((countValue - 1) % imgsArray.length === i) {
-         appImg.src = imgsArray[i];
-         break;
-      } 
-   };
-}
+   const btnsContainer = document.querySelector(".buttons-container");
 
-incrementBtn.addEventListener("click", () => {
-   increase();
-});
-
-// Decrease
-function decrease() {
-
-   if (countValue > 0) {
-      countValue--;
-      count.textContent = countValue;
-
-      for (let i = 0; i < imgsArray.length; i++) {
-         if ((countValue - 1) % imgsArray.length === i) {
-            appImg.src = imgsArray[i];
-            break;
-
-         } else if (countValue === 0) {
-            appImg.src = "./assets/img/letters-wall.png";
-         }     
-      }
+   // Create DOM elements
+   function createNewElement(tagName, className, innerHTML, parent) {
+      const newElement = document.createElement(tagName);
+      newElement.className = className;
+      newElement.innerHTML = innerHTML;
+      parent.append(newElement);
+      return newElement;
    }
-}
 
-decrementBtn.addEventListener("click", () => {
-   decrease();
-});
+   // Minus button
+   const decrementBtn = createNewElement("button", "minus", "-", btnsContainer);
+   
+   // Plus button
+   const incrementBtn = createNewElement("button", "plus", "+", btnsContainer);
+   
+   // Reset button
+   const resetBtn = document.querySelector(".reset");
 
-// Reset
-function reset() {
-   countValue = 0;
-   count.textContent = countValue;
-   const resetSound = new Audio("./assets/audio/demogorgon.mp3");
-   resetSound.volume = 0.083;
-   resetSound.play();
-   appImg.src = "./assets/img/letters-wall.png";  
-}
+   // It opens the app when you click on the "openApp" button
+   function displayApp() {
+      openApp.classList.add("hidden");
+      counterApp.classList.remove("hidden");
+      document.body.style.background = "linear-gradient(to bottom, hsl(217deg    57% 6%), #350a09, hsl(217deg 57% 6%) )";
+      const themeSound = new Audio("./assets/audio/theme.mp3");
+      themeSound.volume = 0.023;
+      themeSound.play();
+   }
 
-resetBtn.addEventListener("click", () => {
-   reset();
-});
-}
+   openApp.addEventListener("click", () => {
+      displayApp();
+   });
+
+   // Update Counter: 
+   counterApp.addEventListener("click", (event) => {
+
+      // Increments the value
+      if (event.target === incrementBtn) {
+         countValue++;
+         count.textContent = countValue;
+
+         // A sound of flickering is heard 
+         const incrementSound = new Audio("./assets/audio/flickering.mp3");
+         incrementSound.play();
+
+         // The image changes, turning on one light bulb at a time
+         for (let i = 0; i < imgsArray.length; i++) {
+            if ((countValue - 1) % imgsArray.length === i) {
+               appImg.src = imgsArray[i];
+               break;
+            } 
+         };
+
+      // Decreases the value
+      } else if (event.target === decrementBtn) {
+         if (countValue > 0) {
+         countValue--;
+         count.textContent = countValue;
+
+            // The image changes, returning to the previous one
+            for (let i = 0; i < imgsArray.length; i++) {
+               if ((countValue - 1) % imgsArray.length === i) {
+                  appImg.src = imgsArray[i];
+                  break;
+
+               } else if (countValue === 0) {
+               appImg.src = "./assets/img/letters-wall.png";
+               }     
+            }
+         }
+      
+      // Reset the value  
+      } else if (event.target === resetBtn) {
+         countValue = 0;
+         count.textContent = countValue;
+         
+         // The sound of the Demogorgon is heard
+         const resetSound = new Audio("./assets/audio/demogorgon.mp3");
+         resetSound.volume = 0.085;
+         resetSound.play();
+
+         // The image returns to the initial one
+         appImg.src = "./assets/img/letters-wall.png";
+      }
+   });
+};
 
 window.onload = () => {
    setUpEvents();
